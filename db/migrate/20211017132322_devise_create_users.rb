f2311a60e3adc34299fb6b985352766d1dce609e
ygorpinto <ygor.pinto@hotmail.com>
2021-10-17 10:23:47 -0300

    ok

--- db/migrate/20211017132322_devise_create_users.rb
+++ db/migrate/20211017132322_devise_create_users.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-class DeviseCreateModels < ActiveRecord::Migration[5.2]
+class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
-    create_table :models do |t|
+    create_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
@@ -32,14 +32,13 @@ class DeviseCreateModels < ActiveRecord::Migration[5.2]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-      t.string :User
 
       t.timestamps null: false
     end
 
-    add_index :models, :email,                unique: true
-    add_index :models, :reset_password_token, unique: true
-    # add_index :models, :confirmation_token,   unique: true
-    # add_index :models, :unlock_token,         unique: true
+    add_index :users, :email,                unique: true
+    add_index :users, :reset_password_token, unique: true
+    # add_index :users, :confirmation_token,   unique: true
+    # add_index :users, :unlock_token,         unique: true
   end
 end
